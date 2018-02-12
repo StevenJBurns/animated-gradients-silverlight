de5e8cf3c98d5a32520c7baa6fd92594a20a723d
stevenjburns@msn.com <stevenjburns@msn.com>
2018-02-12 08:13:56 -0700

    Moved original files from OneDrive to this repo

---
+++ MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace AnimatedGradients
+{
+	public partial class MainPage : UserControl
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
